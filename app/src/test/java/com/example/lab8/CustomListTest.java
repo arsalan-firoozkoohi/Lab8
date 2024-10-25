0db6a01cd961a8a060bd676d65ccc50098b60384
Arsalan Firoozkoohi <181882511+arsalan-firoozkoohi@users.noreply.github.com>
2024-10-24 19:30:27 -0600

    My Final Push

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -59,12 +59,12 @@ public class CustomListTest {
         cityList.deleteCity(city);
         assertFalse(cityList.hasCity(city));
     }
-//
-//    @Test
-//    public void testCountCities(){
-//        CustomList cityList = MockCustomList();
-//        City city = new City("Charlottetown", "Prince Edward Island");
-//        cityList.add(city);
-//        assertEquals(2, cityList.countCities());
-//    }
+
+    @Test
+    public void testCountCities(){
+        CustomList cityList = MockCustomList();
+        City city = new City("Charlottetown", "Prince Edward Island");
+        cityList.addCity(city);
+        assertEquals(1, cityList.countCities());
+    }
 }
